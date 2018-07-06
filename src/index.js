0542c9996fe27673c9d1df6cf4d762f5279320bf
nav123iitr <nav123iitr@gmai.com>
2018-07-06 19:29:42 +0530

    inital commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TableGrid from './TableGrid';
+// import EnhancedTable from './TableUI.jsx';
+    
+import registerServiceWorker from './registerServiceWorker';
+
+
+ReactDOM.render(<TableGrid/>, document.getElementById('root'));
+
